e6fa5fcbdebbd0655c22bfe21ba27058bbb5be09
Sreeram2108 <86720588+Sreeram2108@users.noreply.github.com>
2021-08-30 19:11:54 +0800

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,52 @@
+class Game {
+  constructor() {}
+//reading the gamestate value from the database
+  getState() {
+    var gameStateRef = database.ref("gameState");
+    gameStateRef.on("value", function(data) {
+      gameState = data.val();
+    });
+  }
+//write the gamestate value to the database
+  update(state){
+    database.ref('/').update({
+      gameState:state
+    })
+  }
+
+  start() {
+    player = new Player();
+    playerCount = player.getCount();
+
+    form = new Form();
+    form.display();
+
+    car1 = createSprite(width / 2 - 100, height - 100);
+    car1.addImage(car1_img);
+    car1.scale = 0.07;
+
+    car2 = createSprite(width / 2 + 100, height - 100);
+    car2.addImage(car2_img);
+    car2.scale = 0.07;
+
+    cars = [car1,car2]
+
+  }
+
+  handleElements() {
+    form.hide();
+    form.titleImg.position(40, 50);
+    form.titleImg.class("gameTitleAfterEffect");
+  }
+
+  //!== not equal
+  play() {
+    this.handleElements() 
+    Player.getPlayersInfo();
+    if (allPlayers !== undefined) {
+      image(track, 0, -height * 5, width, height * 6);
+
+      drawSprites();
+    }
+  }
+}
